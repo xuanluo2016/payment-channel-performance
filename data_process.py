9a5e00769fd53f359fbd6a37b99dd3af19abf826
Xuan Luo <xuanluo.lena@gmail.com>
2019-01-18 13:49:40 -0800

    format data extracted from cvs file

--- data_process.py
+++ data_process.py
@@ -42,6 +42,7 @@ right_index = substr.find(']')
 substr = substr[left_index: (right_index +1)]
 print(substr)
 
+
 # test the str.find function
 # teststr = ' var _data = \'[{"ID":302,"gasPrice":2.0,"lastBlock":7078452,"avgTime":65.0,"avgTime2":"00 hr 01 min 05 secs"},{"ID":295,"gasPrice":3.0,"lastBlock":7078452,"avgTime":63.0,"avgTime2":"00 hr 01 min 03 secs"},{"ID":301,"gasPrice":4.0,"lastBlock":7078452,"avgTime":76.0,"avgTime2":"00 hr 01 min 16 secs"},{"ID":196,"gasPrice":5.0,"lastBlock":7078452,"avgTime":64.0,"avgTime2":"00 hr 01 min 04 secs"},{"ID":303,"gasPrice":6.0,"lastBlock":7078452,"avgTime":66.0,"avgTime2":"00 hr 01 min 06 secs"},{"ID":300,"gasPrice":7.0,"lastBlock":7078452,"avgTime":64.0,"avgTime2":"00 hr 01 min 04 secs"},{"ID":313,"gasPrice":8.0,"lastBlock":7078452,"avgTime":23.0,"avgTime2":"00 hr 00 min 23 secs"},{"ID":182,"gasPrice":9.0,"lastBlock":7078452,"avgTime":55.0,"avgTime2":"00 hr 00 min 55 secs"},{"ID":185,"gasPrice":10.0,"lastBlock":7078452,"avgTime":17610.0,"avgTime2":"04 hr 53 min 30 secs"},{"ID":186,"gasPrice":11.0,"lastBlock":7078452,"avgTime":39.0,"avgTime2":"00 hr 00 min 39 secs"},{"ID":188,"gasPrice":12.0,"lastBlock":7078452,"avgTime":14.0,"avgTime2":"00 hr 00 min 14 secs"}]\';\r\n            var _sliderData = '
 # pattern = '['
@@ -52,3 +53,5 @@ print(substr)
 
 
 # read data from json group
+data = json.loads(substr)
+print(data[0]['gasPrice'])
