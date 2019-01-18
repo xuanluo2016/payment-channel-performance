b40bc9d7b2ced40b584d4b440b4faaf2d43f5cb2
Xuan Luo <xuanluo.lena@gmail.com>
2019-01-18 15:17:39 -0800

    successfully extract required information from raw data

--- data_process.py
+++ data_process.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import re
 import json
+import unicodecsv as csv
 
 # remove substrings before and after two characters
 # exmaple:
@@ -12,21 +13,56 @@ def remove_redundant_characters(str, char1, char2):
     right_index = str.find(char2)
     result = str[left_index: (right_index +1)]
     return result
-#####################################################################
 
-file = 'test.csv'
-
-dataframe = pd.read_csv(file, names=['time','safe gas','propose gas','pending tx','gas and time'])
-
-#dataframe.columns = ['time','safe gas','propose gas','pending tx','gas and time']
-#print(dataframe['time'][0])
-
-for item in dataframe['gas and time']:
+# extract gas and waiting time information from raw data
+# input: str by reading csv files
+# output: json format data
+def extract_data(str):
     # extract data as string from csv file
-    result = re.search(' var _data = (.*) var _sliderData = ', item)
-    result = result.group(0)
+    result = re.search('var _data = (.*) var _sliderData = ', str)
+    result = result.group(0)  # type: object
     result = remove_redundant_characters(result, '[', ']')
 
     # extract data as json from string
     result = json.loads(result)
-    print(result[0]['ID'])
+    return result
+
+
+# write data into file
+def write_to_file(file, data):
+    with open(file, 'ab')as csvfile:
+        fieldnames = ['time','pendingTx','gasPrice','avgTime','avgTime2', 'safeGas', 'proposeGas']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,quoting=csv.QUOTE_ALL)
+        writer.writeheader()
+        try:
+            for item in data:
+                assert isinstance(item, object)
+                writer.writerow(item)
+        except Exception as e :
+            print("error when writing data to file")
+            print(e.message)
+    csvfile.close()
+#####################################################################
+
+file = 'test.csv'
+dataframe = pd.read_csv(file, names=['time','safe gas','propose gas','pending tx','gas and time'])
+#dataframe.columns = ['time','safe gas','propose gas','pending tx','gas and time']
+data = []
+
+for index, row in dataframe.iterrows():
+    arr = extract_data(row['gas and time'])
+    for item in arr:
+        temp = {}
+        temp['time'] = row['time']
+        temp['pendingTx'] = row['pending tx']
+        temp['gasPrice'] = item['gasPrice']
+        temp['avgTime'] = item['avgTime']
+        temp['avgTime2'] = item['avgTime2']
+        temp['safeGas'] = row['safe gas']
+        temp['proposeGas'] = row['propose gas']
+        data.append(temp)
+
+# save the data to local file
+write_to_file('test-extracted.csv', data)
+#print(data)
+
