6c3dcbcd6ea4e5813f7c7647cbbec58bf39f1dfe
Xuan Luo <xuanluo.lena@gmail.com>
2019-01-22 16:51:53 -0800

    trying to fix the issue with using web3 to connect to live network

--- test/data_collect.js
+++ test/data_collect.js
@@ -1,3 +1,18 @@
-console.log('test111')
-// var number = web3.eth.blockNumber;
-// console.log(number); // 2744        
+console.log('test111');
+
+var Web3 = require('web3');
+
+// create an instance of web3 using the HTTP provider.
+// NOTE in mist web3 is already available, so check first if it's available before instantiating
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8546"));
+console.log(web3);
+
+var version = web3.version;
+console.log(version); // "0.2.0"
+
+
+var number = web3.eth.blockNumber;
+console.log(number); // 2744        
+
+
+console.log('end');
