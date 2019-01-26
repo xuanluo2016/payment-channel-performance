79e0ff386923fcaf5411f40ac41c8b4614c09004
Xuan Luo <xuanluo.lena@gmail.com>
2019-01-25 16:41:19 -0800

    adding connection pool for fixing the issue with frequent io

--- scripts/webget.js
+++ scripts/webget.js
@@ -1,3 +1,5 @@
+var assert = require('assert');
+
 // return substr between two substrings in a string
 function get_substr(str, substr1, substr2){
     left = str.indexOf(substr1);
@@ -22,50 +24,70 @@ function pausecomp(millis)
 // initlaize the mongodb for storing the data
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
+var mongodb;
+
+// Create the database connection
+MongoClient.connect(url, {  
+    poolSize: 10
+    // other options can go here
+  },function(err, db) {
+      assert.equal(null, err);
+      mongodb = db;
+      }
+  );
+
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("transactions");
+//     //var dbo = db.db("test");
+//     //console.log('start mongo db connnection');
+//     dbo.collection("pending").insertOne(query,function(err, result) {
+//           if (err) throw err;
+//           db.close();
+//         });
+//       });    
+
+
 
 //initlize the websocket 
 const WebSocket = require('ws');
 const ws = new WebSocket('wss://mainnet.infura.io/ws');
 request = ''
 
+
 // store the data
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    //var dbo = db.db("transactions");
-    var dbo = db.db("test");
-    console.log('start mongo db connnection');
-    var query = { item: "canvas", qty: 100, tags: ["cotton"], size: { h: 28, w: 37.5, uom: "cm" }};
-    dbo.collection("inventory").insertOne(query,function(err, result) {
-      if (err) throw err;
-      console.log(result);
-      db.close();
-    });
-  });
+function insert(query){
+    var dbo = mongodb.db("transactions");
+    dbo.collection("pending").insertOne(query,function(err, result) {
+        assert.equal(null, err);
+    });  
+}
 
 
 
-// ws.on('open', function open() {
-//   ws.send('{"jsonrpc":"2.0","method":"eth_newPendingTransactionFilter","params":[],"id":1}');
-// });
+ws.on('open', function open() {
+  ws.send('{"jsonrpc":"2.0","method":"eth_newPendingTransactionFilter","params":[],"id":1}');
+});
 
-// ws.on('message', function incoming(data) {
-//         console.log(data)
-//     if (request != ''){
-//         console.log('asking for filter change')
-//         ws.send(request);
-//         pausecomp(1000);
-//     }else{
-//         console.log('ask for the filter id')
-//         // get the filter id
-//         substr1 = '"result":';
-//         substr2 = '}';
-//         id = get_substr(data, substr1, substr2);
+ws.on('message', function incoming(data) {
+    if (request != ''){
+        //console.log('asking for filter change');
+        query = {"data": data, "time": new Date().toLocaleString(), "seconds":Date.now()};
+        insert(query)
+        ws.send(request);
+        pausecomp(1000);
+    }else{
+        //console.log('ask for the filter id')
+        // get the filter id
+        substr1 = '"result":';
+        substr2 = '}';
+        id = get_substr(data, substr1, substr2);
 
-//         // send request to get pending transactions
-//         str1 = '{"jsonrpc":"2.0","method":"eth_getFilterChanges","params":[';
-//         str2 = '],"id":1}';
-//         request = str1 + id + str2;
-//         console.log(request)
-//         ws.send(request);
-//     }
-// });
+        // send request to get pending transactions
+        str1 = '{"jsonrpc":"2.0","method":"eth_getFilterChanges","params":[';
+        str2 = '],"id":1}';
+        request = str1 + id + str2;
+        //console.log(request)
+        ws.send(request);
+    }
+});
