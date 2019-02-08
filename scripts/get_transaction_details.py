28526a63ae991a74458d8195b2ec83a77a061aef
Xuan Luo <xuanluo.lena@gmail.com>
2019-02-08 14:48:30 -0800

    extract information for transactions

--- scripts/get_transaction_details.py
+++ scripts/get_transaction_details.py
@@ -19,6 +19,9 @@ def parse(source_url,tx_list):
         url = source_url + str(tx)
         item = parse_details(url)
 
+        if(DEBUG):
+            print(item)
+
         # store details into array
         if(item != None):
             results.append(item)
@@ -26,50 +29,78 @@ def parse(source_url,tx_list):
 
 # get the details of a transaction
 def parse_details(url):
-    result = ''
     try:
         response = requests.get(url)
         parser = html.fromstring(response.content)        
 
         # check if the transaction has been mined or not
-        is_mined = (re.search('Success', response.content) != None)
+        is_mined = (re.search('Success', response.content.decode()) != None)
         if(is_mined):
             # for mined transactions, get TimeStamp, Actual Tx Cost, Gas Limit, Gas Price, Gas Used By Transaction
-            result = get_mined_transaction_details(parser)
+            item = get_mined_transaction_details(parser)
 
         else:
-            # for unmined transactions, get Time Last Seen, Time First Seen, Gas Limit, Gas Price 
-            print('not mined')
+            # for unmined transactions, get Time Last Seen, Time First Seen, Gas Limit, Gas Price, Max Fee
+            item = get_unmined_transaction_details(parser)
+        
+        return item
+
 
     except Exception as e:
         print("err in parse_details")
         print(e.message)
 
-    return result
+    return None
+
+def get_transaction_detail(parser, path):
+    result = parser.xpath(path)
+    if(result == None):
+        result = ''
+    return result[0]
 
 def get_mined_transaction_details(parser):
 
     # get TimeStamp
     # list = soup.find('span', id="clock")
-    # print(type(list))
+
     path = '//*[@id="ContentPlaceHolder1_maintable"]/div[8]/text()'
-    timestamp = parser.xpath(path)
-    print(timestamp)
+    timestamp = get_transaction_detail(parser, path)
 
-    actual_cost = ''
+    path = '//*[@id="ContentPlaceHolder1_spanTxFee"]/text()[2]'
+    actual_cost = get_transaction_detail(parser, path)
 
-    gas_limit = ''
+    path = '//*[@id="ContentPlaceHolder1_spanGasLimit"]/text()'
+    gas_limit = get_transaction_detail(parser, path)
 
-    gas_price = ''
+    path = '//*[@id="ContentPlaceHolder1_spanGasPrice"]/text()[2]'
+    gas_price = get_transaction_detail(parser, path)
 
-    gas_used = ''
+    path = '//*[@id="ContentPlaceHolder1_spanGasUsedByTxn"]/text()'
+    gas_used = get_transaction_detail(parser, path)
+
     item = {"timestamp": timestamp, "actual_cost": actual_cost, "gas_limit": gas_limit, "gas_price":gas_price, "gas_used": gas_used }
     
+    return item
 
-    return
+def get_unmined_transaction_details(parser):
+    path = '//*[@id="ContentPlaceHolder1_maintable"]/div[6]/span[2]/text()'
+    time_last_seen = get_transaction_detail(parser, path)
 
-def get_unmined_transaction_details(parse):
-    return
+    path = '//*[@id="ContentPlaceHolder1_maintable"]/div[8]/span/text()'
+    time_first_seen = get_transaction_detail(parser, path)
+
+    path = '//*[@id="ContentPlaceHolder1_spanGasLimit"]/text()'
+    gas_limit = get_transaction_detail(parser, path) 
+
+    path = '//*[@id="ContentPlaceHolder1_spanGasPrice"]/text()'
+    gas_price = get_transaction_detail(parser, path)
+
+    path = '//*[@id="ContentPlaceHolder1_spanTxFee"]/text()[2]'
+    max_fee = get_transaction_detail(parser, path)
+
+    item = {"time_last_seen": time_last_seen, "time_first_seen": time_first_seen, "gas_limit": gas_limit, "gas_price":gas_price, "max_gas_fee": max_fee }
+
+    return item
 ####################### Methods #################################
 
 # get the list of transactions from db processed
