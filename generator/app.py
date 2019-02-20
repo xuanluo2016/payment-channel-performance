082a25a27836fe47ca80c48f6f7adfc6a337a6c0
Xuan Luo <xuanluo.lena@gmail.com>
2019-02-19 17:01:31 -0800

    websocket works properly

--- generator/app.py
+++ generator/app.py
@@ -21,8 +21,9 @@ TRANSACTIONS_TOPIC = os.environ.get('TRANSACTIONS_TOPIC')
 KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL')
 TRANSACTIONS_PER_SECOND = float(os.environ.get('TRANSACTIONS_PER_SECOND'))
 SLEEP_TIME = 1 / TRANSACTIONS_PER_SECOND
+print('sleep_time is: ' + str(SLEEP_TIME))
+request = []
 
-request = ''
 
 # return substr between two substrings in a string
 # return '' if the input is invalid or could not find the substr; else return substr
@@ -41,22 +42,22 @@ def get_substr(str, substr1, substr2) -> str:
 def on_message(ws, message):
     print('----------------------------message------------------')
     print(message)
-    if (request != ''):
+    if (len(request) != 0):
         #print('asking for filter change')
-        query = {"data": data, "time": datetime.now(), "seconds":''}
-        ws.send(request)
+        query = {"data": message, "time": datetime.now(), "seconds":''}
+        ws.send(request[0])
         sleep(SLEEP_TIME)
     else:
         # print('ask for the filter id')
         substr1 = '"result":'
         substr2 = '}'
-        id = get_substr(data, substr1, substr2)
+        id = get_substr(message, substr1, substr2)
 
         # send request to get pending transactions
         str1 = '{"jsonrpc":"2.0","method":"eth_getFilterChanges","params":['
         str2 = '],"id":1}'
-        request = str1 + id + str2
-        ws.send(request)
+        request.append(str1 + id + str2)
+        ws.send(request[0])
     
 
 def on_error(ws, error):
@@ -69,11 +70,11 @@ def on_open(ws):
     def run(*args):
         for i in range(3):
             time.sleep(1)
-            request = ''
+            request = []
             print("------------------------------open---------------------------------")
             ws.send('{"jsonrpc":"2.0","method":"eth_newPendingTransactionFilter","params":[],"id":1}')
-        time.sleep(1)
-        ws.close()
+        # time.sleep(1)
+        # ws.close()
         print("thread terminating...")
     thread.start_new_thread(run, ())
 
@@ -85,15 +86,18 @@ if __name__ == '__main__':
     )
 
     while True:
-        websocket.enableTrace(False)
-        ws = websocket.WebSocketApp("wss://mainnet.infura.io/ws",
-                                    on_message = on_message,
-                                    on_error = on_error,
-                                    on_close = on_close)
-        ws.on_open = on_open
-        ws.run_forever()
+        try: 
+            websocket.enableTrace(False)
+            ws = websocket.WebSocketApp("wss://mainnet.infura.io/ws",
+                                        on_message = on_message,
+                                        on_error = on_error,
+                                        on_close = on_close)
+            ws.on_open = on_open
+            ws.run_forever()
 
-        transaction: dict = create_random_transaction()
-        producer.send(TRANSACTIONS_TOPIC, value=transaction)
-        # print(transaction)  # DEBUG
-        sleep(SLEEP_TIME)
+            transaction: dict = create_random_transaction()
+            producer.send(TRANSACTIONS_TOPIC, value=transaction)
+            # print(transaction)  # DEBUG
+            sleep(SLEEP_TIME)
+        except:
+            pass
