3b7aaeff631f9bf105199a0e5878a600b3cb5540
Xuan Luo <xuanluo.lena@gmail.com>
2019-02-20 16:20:05 -0800

    creating transactions_topic in consumer

--- detector/app.py
+++ detector/app.py
@@ -7,13 +7,7 @@ from kafka import KafkaConsumer, KafkaProducer
 
 KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL')
 RAW_TRANSACTIONS_TOPIC = os.environ.get('RAW_TRANSACTIONS_TOPIC')
-LEGIT_TOPIC = os.environ.get('LEGIT_TOPIC')
-FRAUD_TOPIC = os.environ.get('FRAUD_TOPIC')
-
-
-def is_suspicious(transaction: dict) -> bool:
-    """Determine whether a transaction is suspicious."""
-    return len(transaction) >= 50   
+TRANSACTIONS_TOPIC = os.environ.get('TRANSACTIONS_TOPIC')
 
 if __name__ == '__main__':
     consumer = KafkaConsumer(
@@ -33,6 +27,6 @@ if __name__ == '__main__':
             data = json.loads(value['data'])
             for row in data: 
                 transaction: dict = {'txhash': row, 'time': time, 'seconds': seconds}
-                topic = FRAUD_TOPIC if is_suspicious(transaction) else LEGIT_TOPIC
+                topic = TRANSACTIONS_TOPIC
                 producer.send(topic, value=transaction)
                 print(topic, transaction)  # DEBUG
