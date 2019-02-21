20d354ceba3d6373d2dcad342ae16371e08a9526
Xuan Luo <xuanluo.lena@gmail.com>
2019-02-21 11:58:04 -0800

    getting transaction details

--- detector_transaction_detail/app.py
+++ detector_transaction_detail/app.py
@@ -4,10 +4,12 @@ import json
 import os
 
 from kafka import KafkaConsumer, KafkaProducer
+from transaction_details import parse
 
 KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL')
 TRANSACTIONS_TOPIC = os.environ.get('TRANSACTIONS_TOPIC')
 TRANSACTIONS_DETAILS_TOPIC = os.environ.get('TRANSACTIONS_DETAILS_TOPIC')
+DATASOURCE = os.environ.get('DATASOURCE')
 
 if __name__ == '__main__':
     consumer = KafkaConsumer(
@@ -19,20 +21,28 @@ if __name__ == '__main__':
         bootstrap_servers=KAFKA_BROKER_URL,
         value_serializer=lambda value: json.dumps(value).encode(),
     )
+
+    source_url = 'https://etherscan.io/tx/'
     for message in consumer:
         print('get transaction details')
         transaction: dict = message.value
-        topic = TRANSACTIONS_DETAILS_TOPIC
-        producer.send(topic, value=transaction)
-        print(topic, transaction)  # DEBUG
+        
+        # get transaction details
+        if('txhash' in transaction):
+            print(transaction['txhash'])
+            (item, is_mined) = parse(source_url, transaction['txhash'])
+            topic = TRANSACTIONS_DETAILS_TOPIC
+            transaction: dict = item
+            producer.send(topic, value=transaction)
+            print(topic, transaction)  # DEBUG
 
-        # if('data' in message.value):
-        #     value = message.value
-        #     time = value['time']
-        #     seconds = value['seconds']
-        #     data = json.loads(value['data'])
-        #     for row in data: 
-        #         transaction: dict = {'txhash': row, 'time': time, 'seconds': seconds}
-        #         topic = TRANSACTIONS_TOPIC
-        #         producer.send(topic, value=transaction)
-        #         print(topic, transaction)  # DEBUG
+            # if('data' in message.value):
+            #     value = message.value
+            #     time = value['time']
+            #     seconds = value['seconds']
+            #     data = json.loads(value['data'])
+            #     for row in data: 
+            #         transaction: dict = {'txhash': row, 'time': time, 'seconds': seconds}
+            #         topic = TRANSACTIONS_TOPIC
+            #         producer.send(topic, value=transaction)
+            #         print(topic, transaction)  # DEBUG
