bc6e27ac14afdfe8d55aa33993a69916748b7a88
Xuan Luo <xuanluo.lena@gmail.com>
2019-02-22 12:06:08 -0800

    setup the connection between kafka and spark locally

--- receiver_start_time/app.1.py
+++ receiver_start_time/app.1.py
@@ -1,28 +1,36 @@
 import os
-import findspark
+# import findspark
 # findspark.init('/usr/hdp/2.5.6.0-40/spark')
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 import json
 
-KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL')
 TRANSACTIONS_DETAILS_TOPIC = os.environ.get('TRANSACTIONSTRANSACTIONS_DETAILS_TOPIC_TOPIC')
+# print(os.environ['JAVA_HOME'])
+# os.environ['JAVA_HOME'] = '/usr'
 
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 pyspark-shell'
-sc = SparkContext(appName="PythonSparkStreamingKafka")
-sc.setLogLevel("WARN")
-ssc = StreamingContext(sc,60)
-print('ssc =================== {} {}')
-kafkaStream = KafkaUtils.createStream(ssc,KAFKA_BROKER_URL, 'spark-streaming', {TRANSACTIONS_DETAILS_TOPIC:1})
-print('contexts =================== {} {}')
+#  os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 pyspark-shell'
+
+# Create a local StreamingContext
+sc = SparkContext("local[2]","PythonSparkStreamingKafka")
+
+# Set the batch interval in seconds
+batch_interval = 10
+
+# Create the streaming contect objects
+ssc = StreamingContext(sc,batch_interval)
+
+# Create the kafka connection object
+kafkaStream = KafkaUtils.createStream(ssc, ["starttime"], {"metadata.broker.list": "localhost:9092" ,TRANSACTIONS_DETAILS_TOPIC:1})
+
 lines = kafkaStream.map(lambda x: x[1])
 lines.pprint()
 
 ssc.start()
 ssc.awaitTermination()
 
-
+print("everthing is good")
 
 def decoder(msg):
     baseMessage = json.loads(zlib.decompress(msg[4:]))
--- receiver_start_time/app.py
+++ receiver_start_time/app.py
@@ -1,15 +1,26 @@
-import os
-import findspark
-# findspark.init('/usr/hdp/2.5.6.0-40/spark')
-from pyspark import SparkContext
+
+# Import what we need from PySpark
+from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-import json
+import os 
 
-TRANSACTIONS_DETAILS_TOPIC = os.environ.get('TRANSACTIONSTRANSACTIONS_DETAILS_TOPIC_TOPIC')
+import findspark
+findspark.init()
 
-# Create a local StreamingContext
-sc = SparkContext("local[2]","PythonSparkStreamingKafka")
+# os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka_2.1.1:0.8.2.1 pyspark-shell'
+# os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka_2.11:1.6.3  pyspark-shell'
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.1  pyspark-shell'
+
+# os.environ['PYSPARK_SUBMIT_ARGS'] = '--jars spark-streaming-kafka-assembly_2.10-1.6.0.jar pyspark-shell' #note that the "pyspark-shell" part is very important!!.
+
+# Create a basic configuration
+conf = SparkConf().setAppName("myTestCopyApp")
+
+# Create a SparkContext using the configuration
+sc = SparkContext(conf=conf)
+
+print("=========================test================================")
 
 # Set the batch interval in seconds
 batch_interval = 10
@@ -18,18 +29,15 @@ batch_interval = 10
 ssc = StreamingContext(sc,batch_interval)
 
 # Create the kafka connection object
-kafkaStream = KafkaUtils.createStream(ssc, ["starttime"], {"metadata.broker.list": "localhost:9092" ,TRANSACTIONS_DETAILS_TOPIC:1})
+# kafkaStream = KafkaUtils.createStream(ssc, ["starttime"], {"metadata.broker.list": "localhost:9092" ,"streaming.transactions.details":1})
+
+#  kafkaStream = KafkaUtils.createStream(ssc, 'zookeeper:2181', 'spark-streaming', {'streaming.transactions.details':1})
+# kafkaStream = KafkaUtils.createDirectStream(ssc, ['streaming.transactions.details'], {"metadata.broker.list": "localhost:9092"})
+kafkaStream = KafkaUtils.createStream(ssc, "zookeeper:2181", "spark-streaming", {"streaming.transactions.details":1})
+# kafkaStream = KafkaUtils.createStream(ssc, "zookeeper:2181", "spark-streaming", {"streaming.transactions.details":1})
 
 lines = kafkaStream.map(lambda x: x[1])
 lines.pprint()
 
 ssc.start()
 ssc.awaitTermination()
-
-
-
-def decoder(msg):
-    baseMessage = json.loads(zlib.decompress(msg[4:]))
-    message = {"headers": baseMessage["headers"],
-               "data": b64decode(baseMessage["data"])}
-    return message
