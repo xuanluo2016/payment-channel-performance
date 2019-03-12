ab12e40ac02f354e842d8e5d1684bb4bc555730a
xuanluo2016 <xuanluo2016@139.com>
2019-03-12 14:55:22 -0700

    rename summary to web and add flask application

---
+++ web/app.py
@@ -0,0 +1,39 @@
+
+from flask import Flask, render_template, request
+from get_data import get_summary_all,write_to_file
+
+
+app = Flask(__name__)
+
+
+# @app.route("/,  methods=['GET', 'POST']")
+@app.route("/")
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route("/summary", methods=['GET'])
+def summary(num):
+    if(num == 0):
+        return get_summary_all()
+    else:
+        results = get_summary_all()
+        return results[0,num]
+        
+if __name__ == "__main__":
+    
+    # Save a copy of summary db whenever the summary container restarts
+    file = '/data/test/data.json'
+    results = get_summary_all()
+    write_to_file(file, results)
+    
+    # Start flask application with access from localhost
+    app.run(host='0.0.0.0')
+
+
+
+
+
+
+
+
+
