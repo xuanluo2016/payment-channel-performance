ef0b76af6230e21507e6f638d73873de1a870f6f
xuan <xuanluo.lena@gmail.com>
2019-03-20 13:51:33 -0700

    detector for pending tx from etherscan is working

--- detector/app.py
+++ detector/app.py
@@ -4,8 +4,8 @@ import json
 import os
 from time import sleep
 
-
 from kafka import KafkaConsumer, KafkaProducer
+from get_transactions import get_transactions
 
 KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL')
 RAW_TRANSACTIONS_TOPIC = os.environ.get('RAW_TRANSACTIONS_TOPIC')
@@ -27,17 +27,10 @@ if __name__ == '__main__':
             time = value['time']
             seconds = value['seconds']
             data = value['data']
-            # when there is only one transaction detected
-            if(len(data) == 66):
-                transaction: dict = {'txhash': data, 'starttime': time, 'seconds': seconds}
+            transactions = get_transactions(data.encode())
+            for transaction in transactions:
+                # transaction: dict = {'txhash': data, 'starttime': time, 'seconds': seconds}
                 topic = TRANSACTIONS_TOPIC
                 producer.send(topic, value=transaction)
                 print(topic, transaction)  # DEBUG
-            
-            # when there are more than one transaction detected     
-            else:
-                for row in data: 
-                    transaction: dict = {'txhash': row, 'starttime': time, 'seconds': seconds}
-                    topic = TRANSACTIONS_TOPIC
-                    producer.send(topic, value=transaction)
-                    print(topic, transaction)  # DEBUG
+
--- detector/get_transactions.py
+++ detector/get_transactions.py
@@ -2,24 +2,30 @@ import requests
 import json
 from lxml import html
 import re
+from datetime import datetime
+import dateutil.parser
 
 
 # get the details of a transaction
-def get_transactions(content, time):
+def get_transactions(content):
     parser = html.fromstring(content)
     XPATH_PAGEINFO = '//*[@id="transfers"]/div[2]/table/tbody/tr'
     pageInfo = parser.xpath(XPATH_PAGEINFO)
-
-        for info in pageInfo:
+    
+    results = []    
+    for info in pageInfo:
         list = info.xpath('./td[1]/span/a/text()')
         if(len(list) > 0):
-                txhash = str(list[0])
+            txhash = str(list[0])
 
         list = info.xpath('./td[3]/span/@title')
         if(len(list) > 0):
-                time = list[0]
-                start_time = str(time)
-                start_time = dateutil.parser.parse(start_time)
-                epoch = start_time.strftime('%s')
-                
-        return(txhash, start_time, epoch)
+            time = list[0]
+            start_time = str(time)
+            datetime = dateutil.parser.parse(start_time)
+            epoch = datetime.strftime('%s')
+
+        if(txhash != None) and (start_time != None) and (epoch != None):
+            item = {'txhash': txhash, 'starttime': start_time, 'seconds': epoch}
+            results.append(item)
+    return results
