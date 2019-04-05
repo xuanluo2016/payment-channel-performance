77b4232eadee7fd6cd8f0c23becaf098c290a4e4
Xuan Luo <xuanluo.lena@gmail.com>
2019-04-05 14:11:34 -0700

    adding concurrent works for queue

--- generator_fullnode/app.py
+++ generator_fullnode/app.py
@@ -3,7 +3,9 @@ import mysql.connector
 import requests
 import queue
 import time
+import threading
 
+# Send request to get pending transactions
 def send_request(url):
   headers = {'content-type': 'application/json'}
   data = '{"method":"parity_pendingTransactions","params":[],"id":1,"jsonrpc":"2.0"}'
@@ -12,7 +14,8 @@ def send_request(url):
   result = response.content
   return result
 
-def get_pendingtransactions(data):
+# Extract pending transaction list
+def get_pendingtransactions(data, starttime):
   data = json.loads(data.decode())
   results = []
   if('result' in data):
@@ -22,63 +25,84 @@ def get_pendingtransactions(data):
       temp.append(row['hash'])
       temp.append(row['gasPrice'])
       temp.append(row['gas'])
-      temp.append(0)
-
-      # item = {'hash': row['hash'],'gasprice':row['gasPrice'],'gas':row['gas']}
-      # item = {row['hash'],row['gasPrice'],row['gas'],0}
-      # results.append(item)
+      temp.append(starttime)
       results.append(temp)
 
   return results  
 
-# def insert_txlist():
-#   return
 
 def main():
+  def worker_push():
+      print('worker push started')
+
+      # Setup connection url
+      url = 'http://35.162.229.77:8545'
+      #url = 'http://localhost:8545'
+
+      # Insert complete transaction list into queue
+      oldhash = ''
+      count = 0
+      while True:
+        starttime = time.time()
+        # Get response from http request
+        data = send_request(url)
+        newhash = hash(data)
+        if(newhash != oldhash):
+          q.put({'data':data, 'starttime':starttime})
+          count = count + 1
+          print('pushed items: ', count)
+          oldhash = newhash
+        time.sleep(1)
+
+  def worker_pull():
+    print('worker pull started')
+    # DB initialization
+    mydb = mysql.connector.connect(
+      host = "ethfullnodedb.c0cwkssklnbh.us-west-2.rds.amazonaws.com",
+      user = "admin",
+      passwd = "l3ft0fth3d0t",
+      database = "transactionsdb"
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE TABLE IF NOT EXISTS transactions (txhash VARCHAR(255) PRIMARY KEY, gasprice VARCHAR(255) NOT NULL, gas VARCHAR(255), starttime FLOAT(50,7), Index(txhash))")
+    mydb.commit()
+
+    while True:
+      item = q.get()
+      if item is None:
+        print('no item in the queue')
+        break
+      # Extract useful data from request
+      txlist = get_pendingtransactions(item['data'],item['starttime'])
+      # Insert every single transaction into table transadtions
+      sql_insert_query =  "INSERT IGNORE INTO transactions (txhash, gasprice, gas, starttime) VALUES  (%s, %s, %s,%s)"
+      mycursor.executemany(sql_insert_query, txlist)  
+      mydb.commit()
+      print("affected rows = {}".format(mycursor.rowcount))
+    
+    mydb.close()
+    q.task_done()
+
   # Create a fifo qeque
-  q = queue.Queue() 
+  q = queue.Queue()
+  
+  threads = []
 
-  # Setup connection url
-  url = 'http://35.162.229.77:8545'
-  #url = 'http://localhost:8545'
+  t = threading.Thread(target=worker_push)
+  t.start()
+  threads.append(t)
 
-  # Get response from http request
-  data = send_request(url)
+  t = threading.Thread(target=worker_pull)
+  t.start()
+  threads.append(t)
 
-  # Extract useful data from request
-  start = time.time()
-  txlist = get_pendingtransactions(data)
-  end = time.time()
-  print(end - start, "delay in extracting txlist")
-  # print(txlist)
+  # block until all tasks are done
+  q.join()
 
-  print("test db connection")
-
-  # DB initialization
-  mydb = mysql.connector.connect(
-    host = "ethfullnodedb.c0cwkssklnbh.us-west-2.rds.amazonaws.com",
-    user = "admin",
-    passwd = "l3ft0fth3d0t",
-    database = "transactionsdb"
-  )
-
-  mycursor = mydb.cursor()
-  mycursor.execute("CREATE TABLE IF NOT EXISTS txlist (txhashlist VARCHAR(255) NOT NULL, starttime FLOAT )")
-  mycursor.execute("CREATE TABLE IF NOT EXISTS transactions (txhash VARCHAR(255) NOT NULL, gasprice VARCHAR(255) NOT NULL, gas VARCHAR(255), starttime FLOAT)")
-
-  # mycursor.execute("SHOW TABLES")  
-  # mycursor.execute("SELECT * FROM txlist")
-
-  # Insert transaction lists into table txlist
-  print(txlist, 'txlist')
-  sql_insert_query =  "INSERT INTO transactions (txhash, gasprice, gas, starttime) VALUES  (%s, %s, %s,%s)"
-  mycursor.executemany(sql_insert_query, txlist)  
-  mydb.commit()
-
-  mycursor.execute("SELECT * FROM transactions")
-
-  for x in mycursor:
-    print(x)
+  # # stop workers
+  # for t in threads:
+  #     t.join()
 
 if __name__== "__main__":
     main()
