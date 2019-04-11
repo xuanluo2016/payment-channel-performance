4641a83d1fef3a4cfe27345d3554219f105f75fb
xuanluo2016 <xuanluo.lena@gmail.com>
2019-04-11 06:09:03 +0000

    correct the port for web3

--- generator_fullnode/test.js
+++ generator_fullnode/test.js
@@ -2,7 +2,7 @@ const Web3 = require("web3");
 
 // "Web3.providers.givenProvider" will be set if in an Ethereum supported browser.
 // const web3 = new Web3("ws://ec2-35-162-229-77.us-west-2.compute.amazonaws.com:8545");
-const web3 = new Web3("ws://localhost:8545");
+const web3 = new Web3("ws://localhost:8546");
 
 // const web3 = new Web3("http://35.162.229.77:8545")
 
