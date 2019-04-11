281a908fa87f1ae69af2ab6604ff07a61e3c5c0c
xuan <xuanluo.lena@gmail.com>
2019-04-10 23:28:25 -0700

    saving data to local file

--- generator_fullnode/test.js
+++ generator_fullnode/test.js
@@ -1,14 +1,22 @@
 const Web3 = require("web3");
+var fs = require("fs");
+
 
 // "Web3.providers.givenProvider" will be set if in an Ethereum supported browser.
 // const web3 = new Web3("ws://ec2-35-162-229-77.us-west-2.compute.amazonaws.com:8545");
+// const web3 = new Web3("http://35.162.229.77:8545")
 const web3 = new Web3("ws://localhost:8546");
 
-// const web3 = new Web3("http://35.162.229.77:8545")
+
+
 
 web3.eth.subscribe('pendingTransactions', function(error, result) {
     console.log(error);
 })
 .on('data', function(txData){
-    web3.eth.getTransaction(txData).then(console.log);
+    fs.writeFile('hello.txt', web3.eth.getTransaction(txData), function(err) {
+        // If an error occurred, show it and return
+        if(err) return console.error(err);
+        // Successfully wrote to the file!
+      });
 });
