3ba8acb484285106beef675ce3168977074d3aa4
Ubuntu <ubuntu@127.0.1.1ip-10-1-2-244>
2019-04-11 07:37:26 +0000

    rename test to data collect script

--- generator_fullnode/data_collect.js
+++ generator_fullnode/data_collect.js
@@ -13,14 +13,16 @@ web3.eth.subscribe('pendingTransactions', function(error, result) {
 })
 .on('data', function(txData){
     web3.eth.getTransaction(txData).then(function(value) {
-       // console.log(value, Math.floor(new Date() / 1000))
+       // console.log(value, ',' , Math.floor(new Date() / 1000))
        var dict = {};
        dict.data = value
        dict.starttime = Math.floor(new Date() / 1000)	
-       fs.writeFile('hello.json', JSON.stringify(dict), 'utf8', function(err) {
+       console.log(dict)
+
+	// fs.writeFile('hello.json', JSON.stringify(dict), 'utf8', function(err) {
        // If an error occurred, show it and return
-       if(err) return console.error(err);
+      // if(err) return console.error(err);
        // Successfully wrote to the file!
-           });
+         //  });
       })
 });
