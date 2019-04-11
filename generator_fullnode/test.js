d7b8534501fc2460d774c9ac447527a9a7446131
xuan <xuanluo.lena@gmail.com>
2019-04-10 22:59:18 -0700

    adding web3 subscribe for app side

---
+++ generator_fullnode/test.js
@@ -0,0 +1,14 @@
+const Web3 = require("web3");
+
+// "Web3.providers.givenProvider" will be set if in an Ethereum supported browser.
+// const web3 = new Web3("ws://ec2-35-162-229-77.us-west-2.compute.amazonaws.com:8545");
+const web3 = new Web3("ws://localhost:8545");
+
+// const web3 = new Web3("http://35.162.229.77:8545")
+
+web3.eth.subscribe('pendingTransactions', function(error, result) {
+    console.log(error);
+})
+.on('data', function(txData){
+    web3.eth.getTransaction(txData).then(console.log);
+});
